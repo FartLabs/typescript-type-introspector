5c4fd05d41f68f8f95b799b6f80a172e7d26fbd5
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-11-02 17:08:50 -0700

    wip

--- example.ts
+++ example.ts
@@ -1,17 +1,24 @@
-import { fromJSONLd } from "#/lib/from-jsonld/from-jsonld.ts";
+import { Store } from "n3";
+import { JsonLdParser } from "jsonld-streaming-parser";
+import { promisifyEventEmitter } from "event-emitter-promisify";
 
 const doc = {
-  "@context": {
-    "name": "http://schema.org/name",
-    "description": "http://schema.org/description",
-  },
-  "@id": "http://example.org",
-  "name": "Example",
-  "description": "An example document.",
+  "@context": "http://schema.org/",
+  "@type": "Person",
+  "name": "Jane Doe",
+  "jobTitle": "Professor",
+  "telephone": "(425) 123-4567",
+  "url": "http://www.janedoe.com",
 };
 
 // deno run -A example.ts
 if (import.meta.main) {
-  const nquads = await fromJSONLd(doc);
-  console.log({ nquads });
+  const store = new Store();
+  const parser = new JsonLdParser();
+  parser.write(JSON.stringify(doc));
+  parser.end();
+  await promisifyEventEmitter(store.import(parser));
+
+  // Logs all the quads in the store
+  console.log(...store);
 }
