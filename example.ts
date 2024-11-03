9370a7651418b0b172281497f6c4a6775003e5e1
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-11-02 18:55:25 -0700

    add module storeJSONLd

--- example.ts
+++ example.ts
@@ -1,24 +1,20 @@
-import { Store } from "n3";
-import { JsonLdParser } from "jsonld-streaming-parser";
-import { promisifyEventEmitter } from "event-emitter-promisify";
-
-const doc = {
-  "@context": "http://schema.org/",
-  "@type": "Person",
-  "name": "Jane Doe",
-  "jobTitle": "Professor",
-  "telephone": "(425) 123-4567",
-  "url": "http://www.janedoe.com",
-};
+import { n3 } from "#/lib/n3/mod.ts";
+import { storeJSONLd } from "#/lib/store-jsonld/mod.ts";
 
 // deno run -A example.ts
 if (import.meta.main) {
-  const store = new Store();
-  const parser = new JsonLdParser();
-  parser.write(JSON.stringify(doc));
-  parser.end();
-  await promisifyEventEmitter(store.import(parser));
+  const store = new n3.Store();
+  await storeJSONLd(store, {
+    "@context": "http://schema.org/",
+    "@type": "Person",
+    "name": "Jane Doe",
+    "jobTitle": "Professor",
+    "telephone": "(425) 123-4567",
+    "url": "http://www.janedoe.com",
+  });
 
-  // Logs all the quads in the store
-  console.log(...store);
+  // Logs all the quads in the store.
+  for (const quad of store) {
+    console.log(quad);
+  }
 }
---
+++ lib/store-jsonld/store-jsonld.ts
@@ -0,0 +1,20 @@
+import { JsonLdParser } from "jsonld-streaming-parser";
+import { promisifyEventEmitter } from "event-emitter-promisify";
+import { n3 } from "#/lib/n3/mod.ts";
+import { jsonld } from "#/lib/jsonld/mod.ts";
+
+/**
+ * storeJSONLd stores a JSON-LD document in a N3 store.
+ *
+ * @see
+ * https://github.com/rubensworks/jsonld-streaming-parser.js/blob/b55134f2c5c938c3cdf73981667ce090655dff9b/README.md#convert-a-json-ld-string-to-an-rdfjs-dataset
+ */
+export async function storeJSONLd(
+  store: n3.Store,
+  doc: jsonld.JsonLdDocument,
+): Promise<void> {
+  const parser = new JsonLdParser();
+  parser.write(JSON.stringify(doc));
+  parser.end();
+  await promisifyEventEmitter(store.import(parser));
+}
